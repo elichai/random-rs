4a894fc9cee8c742c05774688d570bc78d52b152
Elichai Turkel <elichai.turkel@gmail.com>
2019-08-25 19:44:00 -0400

    Added primitives support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
-use core::mem;
+use core::{char, mem};
+
 pub enum Error {
     Something,
 }
@@ -46,10 +47,72 @@ pub trait Random {
         unsafe { mem::transmute(buf) }
     }
 
-    // TODO: check compiler version.
-    //    fn get_u128(&mut self) -> u128 {
-    //        let mut buf = [0u8; 16];
-    //        self.fill_bytes(&mut buf);
-    //        unsafe { mem::transmute(buf) }
-    //    }
+    #[cfg(feature = "u128")]
+    fn get_u128(&mut self) -> u128 {
+        let mut buf = [0u8; 16];
+        self.fill_bytes(&mut buf);
+        unsafe { mem::transmute(buf) }
+    }
+
+    // TODO: More research, least/most significant bit?
+    fn get_bool(&mut self) -> bool {
+        let bit = self.get_u8() & 0b1000_0000;
+        debug_assert!(bit < 2);
+        bit == 1
+    }
 }
+
+
+// Will overflow(i.e. sign extend) correctly https://doc.rust-lang.org/nomicon/casts.html.
+// should only be used with the same type.
+macro_rules! impl_generate_rand_ifromu {
+    ($ity:ty, $uty: ty) => {
+        impl GenerateRand for $ity {
+            #[inline]
+            fn generate<R: Random + ?Sized>(rand: &mut R) -> Self {
+                debug_assert_eq!(mem::size_of::<$ity>(), mem::size_of::<$uty>());
+                <$uty>::generate(rand) as $ity
+            }
+        }
+    }
+}
+
+
+impl GenerateRand for u8 {
+    fn generate<R: Random + ?Sized>(rand: &mut R) -> Self {
+        rand.get_u8()
+    }
+}
+
+impl GenerateRand for u16 {
+    fn generate<R: Random + ?Sized>(rand: &mut R) -> Self {
+        rand.get_u16()
+    }
+}
+
+impl GenerateRand for u32 {
+    fn generate<R: Random + ?Sized>(rand: &mut R) -> Self {
+        rand.get_u32()
+    }
+}
+
+impl GenerateRand for u64 {
+    fn generate<R: Random + ?Sized>(rand: &mut R) -> Self {
+        rand.get_u64()
+    }
+}
+
+#[cfg(feature = "u128")]
+impl GenerateRand for u128 {
+    fn generate<R: Random + ?Sized>(rand: &mut R) -> Self {
+        rand.get_u128()
+    }
+}
+
+
+impl_generate_rand_ifromu!{i8, u8}
+impl_generate_rand_ifromu!{i16, u16}
+impl_generate_rand_ifromu!{i32, u32}
+impl_generate_rand_ifromu!{i64, u64}
+#[cfg(feature = "u128")]
+impl_generate_rand_ifromu!{i128, u128}
